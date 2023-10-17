0c80181bf415cd8af0a82f6e167d7bbe439b51c6
josemarzioni <jose.marzioni1@gmail.com>
2023-10-17 14:11:39 -0300

    tercer commit

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@ let cad=`
         <a class="b1" href="./voluntariado.html">Participá</a>
         <a class="b1" href="./QuienesSomos.html">Quienes Somos</a>
         <a class="b1" href="./gala.html">Gala 2023</a>
-        <a class="b1" href="./tienda.html">Tienda</a>
     </nav>
 `
 document.querySelector("header").innerHTML=cad
